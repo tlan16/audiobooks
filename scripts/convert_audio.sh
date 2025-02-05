447f4bd84169e303f6274ced3a025ebbf670aedd
Frank Lan <frank.lan@rea-group.com>
2025-02-05 16:29:20 +1100

    1

--- scripts/convert_audio.sh
+++ scripts/convert_audio.sh
@@ -6,9 +6,13 @@
 
 set -e
 
+# Shell colors
+RED='\033[0;31m'
+GREEN='\033[32m'
+NC='\033[0m' # No Color
 
-echo "Input: $(realpath "$1")"
-echo "Output: $(realpath "$2")"
+echo "${GREEN}Input: $(realpath "$1")"
+echo "${GREEN}Output: $(realpath "$2")"
 
 INPUT_ENV=$(ffprobe -v error -select_streams v:0 -show_entries stream=codec_name -of default=nokey=1:noprint_wrappers=1 "${1}")
 OUTPUT_EXT="${2##*.}"
@@ -16,11 +20,12 @@ TEMP_FILE=/tmp/"$(cat /dev/urandom | gtr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 
 
 # validation
 if [ "${OUTPUT_EXT}" = "" ]; then
-  echo "Invalid output of ${2}, skipping ..."
+  echo "${RED}Invalid output of ${2}, skipping ...${NC}"
   return 1
 fi
 
-echo "Start converting "$(basename "$1")" to temp file: ${TEMP_FILE}"
+echo "${GREEN}Start converting "$(basename "$1")" to temp file: ${TEMP_FILE}"
+echo "${NC}"
 
 ffmpeg -hide_banner \
   -i "${1}" \
@@ -29,7 +34,7 @@ ffmpeg -hide_banner \
   -application:a voip \
   -b:a 20k \
   -vbr 2 \
-  -compression_level 6 \
+  -compression_level 10 \
   -y \
   "${TEMP_FILE}"
 
@@ -37,5 +42,5 @@ mv "${TEMP_FILE}" "${2}"
 
 #if [ "${1}" != "${2}" ]; then
 #  rm -f "${1}"
-#  echo "Deleted source file" "$(realpath "$1")"
+#  echo "${GREEN}Deleted source file" "$(realpath "$1")"
 #fi
